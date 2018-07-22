f5168ec1e2243fb6e3641fc5efe77a09690bb408
Vitaly Fedorov <fedvit@yandex.ru>
2018-07-22 23:16:04 +0300

    before responsive design changes

--- js/app.js
+++ js/app.js
@@ -43,73 +43,51 @@ function mainFunction() {
  updateStars();
 
 	if (!card.classList.contains('open') && !card.classList.contains('show')) {
+ 		openCards.push(card);
+ 		card.classList.add('open', 'show');
+ 		movesTotal.push(moves);
+ 		const totalMoves = document.querySelector('.moves');
+ 		totalMoves.textContent = movesTotal.length;
 
+		if (movesTotal.length <= 1) {
+			 startTimer();
+		 };
 
-	console.log(card);
-
-
- 	openCards.push(card);
- 	card.classList.add('open', 'show');
- 	//console.log('Open Cards:', openCards.length);
- 	movesTotal.push(moves);
- 	//console.log('Moves Total:', movesTotal.length);
- 	const totalMoves = document.querySelector('.moves');
- 	totalMoves.textContent = movesTotal.length;
-
-
-//first card
-	if (movesTotal.length <= 1) {
-		 startTimer();
-	 };
-
-
-
-
- 	 if (openCards.length == 2) {
- 	 	if (openCards[0].innerHTML == openCards[1].innerHTML) {
-
-		console.log("match correct!");
-	 				openCards[0].classList.add('match', 'open', 'show');
-	 				openCards[1].classList.add('match', 'open', 'show');
-
-	 				matchedCards.push(openCards[0]);
-					matchedCards.push(openCards[1]);
-
-	//				console.log("matchedCards.length", matchedCards.length);
-	// 				console.log("document.getElementsByClassName('match').length", document.getElementsByClassName('match').length)																										;
-					openCards = [];
-	 			} else {
-
-	 				setTimeout(function() {
-	 					openCards.forEach(function(card) {
- 						card.classList.remove('open', 'show');
- 					});
- 					openCards = [];
- 				}, 1000);
- 			}
- 		}
- 	}
-
- 	if (document.getElementsByClassName('match').length == 16 ) {
- 		endGame();
- 	}
+	 	if (openCards.length == 2) {
+	 	 	if (openCards[0].innerHTML == openCards[1].innerHTML) {
+		 				openCards[0].classList.add('match', 'open', 'show');
+		 				openCards[1].classList.add('match', 'open', 'show');
+		 				matchedCards.push(openCards[0]);
+						matchedCards.push(openCards[1]);
+						openCards = [];
+		 	}
+			else {
+		 				setTimeout(function() {
+	 						openCards.forEach(function(card) {
+	 						card.classList.remove('open', 'show');
+	 						});
+	 						openCards = [];
+	 					}, 500);
+	 		}
+	 	}
+	}
+	if (matchedCards.length == 16) {
+		endGame();
+	}
 };
 
-
-
-
- function updateStars() {
+function updateStars() {
  if (movesTotal.length <= 1) {
  	document.querySelector('.stars').style.color = "yellow";
- 		} else if (movesTotal.length >= 2 && movesTotal.length <= 3) {
+} else if (movesTotal.length >= 17 && movesTotal.length <= 20) {
  			document.querySelector('#star-one').style.color = "gray";
- 				} else if (movesTotal.length >= 4 && movesTotal.length <= 5) {
+		} else if (movesTotal.length >= 21 && movesTotal.length <= 24) {
  				document.querySelector('#star-two').style.color = "gray";
- 					} else if (movesTotal.length >= 6 && movesTotal.length <= 7) {
+			} else if (movesTotal.length >= 25 && movesTotal.length <= 28) {
  					document.querySelector('#star-three').style.color = "gray";
- 						} else if (movesTotal.length >= 8 && movesTotal.length <= 9) {
+				} else if (movesTotal.length >= 29 && movesTotal.length <= 32) {
  						document.querySelector('#star-four').style.color = "gray";
- 							} else if (movesTotal.length >= 10) {
+					} else if (movesTotal.length >= 33) {
  							document.querySelector('#star-five').style.color = "gray";
  							}
  }
@@ -117,47 +95,50 @@ function mainFunction() {
 
 
  function startTimer () {
-   var sec = 0;
+   let sec = 0;
     function pad ( val ) {
       return val > 9 ? val : "0" + val;
     }
-    setInterval(function(){
+  		setInterval(function(){
       document.getElementById("sec").innerHTML=pad(++sec%60);
       document.getElementById("min").innerHTML=pad(parseInt(sec/60,10));
     }, 1000);
  }
 
- //When game finishes and modal appears
-
  function endGame () {
-   const modal = document.querySelector(".modal");
-   modal.style.display = "block";
 
-   let totalSeconds = document.getElementById("sec").innerHTML;
-   let totalMinutes = document.getElementById("min").innerHTML;
+   const results = document.getElementById("results");
+   results.style.visibility = "visible";
 
-   finalSeconds.innerHTML = `${totalSeconds}`;
-   finalMinutes.innerHTML = `${totalMinutes}`;
+   let resultSeconds = document.getElementById("sec").innerHTML;
+   let resultMinutes = document.getElementById("min").innerHTML;
 
-   let totalMoves = movesTotal.length;
-   movesFinal.innerHTML = `${totalMoves}`;
-   if (totalMoves <= 20) {
-     starTotal.innerHTML = '3';
-   } else if(totalMoves >= 35 && totalMoves <= 20) {
-     starTotal.innerHTML = '2';
+   finalSeconds.innerHTML = `${resultSeconds}`;
+   finalMinutes.innerHTML = `${resultMinutes}`;
+
+   let resultMoves = movesTotal.length;
+   movesFinal.innerHTML = `${resultMoves}`;
+   if (totalMoves < 17) {
+     starTotal.innerHTML = '5';
+   } else if(totalMoves >=17 && totalMoves <= 20) {
+     starTotal.innerHTML = '4';
+	 } else if(totalMoves >=21 && totalMoves <= 24) {
+		 starTotal.innerHTML = '3';
+	 } else if(totalMoves >=25 && totalMoves <= 28) {
+		 starTotal.innerHTML = '2';
+	 } else if(totalMoves >=29 && totalMoves <= 32) {
+		 starTotal.innerHTML = '1';
    } else {
-     starTotal.innerHTML = '1';
+     starTotal.innerHTML = '0';
    }
  }
-
 
  function restartGame(){
    window.location.href = window.location.href;
  }
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
+    let currentIndex = array.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
