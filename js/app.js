459aa9691ad763141410979ac79fa9058638c871
Vitaly Fedorov <fedvit@yandex.ru>
2018-07-22 09:45:27 +0300

    try to combine

--- js/app.js
+++ js/app.js
@@ -3,6 +3,155 @@
  */
 
 
+
+ const cards = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o', 'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'];
+
+
+
+ //guidance for dynamically generating cards https://www.vitoshacademy.com/javascript-create-ul-and-li-elements-with-js-with-chained-function/
+ let ul = document.createElement('ul');
+ document.getElementById('template').appendChild(ul);
+ ul.classList.add('deck');
+
+   cards.forEach(function(card) {
+
+     let li = document.createElement('li');
+     ul.appendChild(li);
+     li.classList.add('card');
+
+     let i = document.createElement('i');
+     li.appendChild(i);
+     for (var j = 0; j < cards.length; j++) {
+     i.classList.add('fa', card);
+   }
+ });
+
+ let allCards = document.querySelectorAll('.card');
+ let openCards = [];
+ let matchedCards = [];
+ let moves = 0;
+ let movesTotal = [];
+
+ 
+
+ function startGame () {
+   shuffle(cards);
+ }
+
+ //Timer function from https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
+
+ function timer () {
+ var sec = 0;
+ function pad ( val ) { return val > 9 ? val : "0" + val; }
+ setInterval(function(){
+     document.getElementById("sec").innerHTML=pad(++sec%60);
+     document.getElementById("min").innerHTML=pad(parseInt(sec/60,10));
+ }, 1000);
+
+ }
+
+ //When game finishes and modal appears
+
+ function endGame () {
+
+   const modal = document.querySelector(".modal");
+
+   modal.style.display = "block";
+
+   let totalSeconds = document.getElementById("seconds").innerHTML;
+   let totalMinutes = document.getElementById("minutes").innerHTML;
+
+   finalSeconds.innerHTML = `${totalSeconds}`;
+   finalMinutes.innerHTML = `${totalMinutes}`;
+
+   let totalMoves = movesTotal.length;
+   movesFinal.innerHTML = `${totalMoves}`;
+   if (totalMoves <= 20) {
+     starTotal.innerHTML = '3';
+   } else if(totalMoves >= 35 && totalMoves <= 20) {
+     starTotal.innerHTML = '2';
+   } else {
+     starTotal.innerHTML = '1';
+   }
+ }
+
+ //restart and reset inspired from https://stackoverflow.com/questions/6666363/is-it-possible-to-clear-a-form-and-reset-reload-the-page-with-one-button
+
+ document.querySelector(".restart").addEventListener("click", restartGame);
+
+ function restartGame(){
+   window.location.href = window.location.href;
+ }
+
+ reset.addEventListener("click", resetGame);
+
+ function resetGame() {
+   window.location.href = window.location.href;
+ }
+
+ startGame();
+
+
+
+ //thankful for this webinar that helped me get started: https://www.youtube.com/watch?v=_rUH-sEs68Y
+
+
+ allCards.forEach(function(card){
+
+   card.addEventListener('click', function(e) {
+     if (!card.classList.contains('open') && !card.classList.contains('show')) {
+     openCards.push(card);
+     card.classList.add('open', 'show');
+     console.log('Open Cards:', openCards.length);
+     movesTotal.push(moves);
+     console.log(movesTotal.length);
+     const totalMoves = document.querySelector('.moves');
+     totalMoves.textContent = movesTotal.length;
+
+       if (movesTotal.length <= 1) {
+         timer();
+         const yellowStars = document.querySelector('.stars');
+         yellowStars.style.color = '#FFA500';
+
+       } else if (movesTotal.length >= 20 && movesTotal.length <=35) {
+
+             document.getElementById('star1').style.color = 'grey';
+           } else if (movesTotal.length >= 36  ){
+
+             document.getElementById('star2').style.color = 'grey';
+       }
+
+       if (openCards.length == 2) {
+
+         if (openCards[0].innerHTML == openCards[1].innerHTML) {
+           console.log("match");
+           openCards[0].classList.add('match', 'open', 'show');
+           openCards[1].classList.add('match', 'open', 'show');
+           openCards = [];
+
+           matchedCards.push(card);
+           console.log(matchedCards.length);
+           console.log(document.getElementsByClassName('match').length);
+
+         } else {
+
+           setTimeout(function() {
+             openCards.forEach(function(card) {
+               card.classList.remove('open', 'show');
+             });
+             openCards = [];
+           }, 700);
+         }
+       }
+     }
+
+     if (document.getElementsByClassName('match').length == 16 ) {
+       endGame();
+     }
+   });
+ });
+
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
